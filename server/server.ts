1f4a60d84ae1e5c4f11ddd4bf5d9f69d0d4eb2ed
mohamedameurkoidja <akoija12@gmail.com>
2024-01-03 17:05:21 +0100

    100%

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,23 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
-import favotitRouter from "./router/favorite_route.ts"
-// import basket  from "../server/router/basket"
+import {router} from "./controller/stripe"
 config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+import favotitRouter from "./router/favorite_route"
+// import stripe from "./router/stripe_route"
+// import basket  from "../server/router/basket"
+
+
+
 
 app.use("/favorit",favotitRouter )
 // app.use("/baskets", basket);
-
-
+// app.use("/stripe",stripe)
+app.use("/api",router)
 
 app.listen(process.env.PORT, () => {
   console.log(
